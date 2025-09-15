aa304893d01d80f410aa06b06de4e777d1904b70
Clément Joly <foss@131719.xyz>
2025-09-15 23:12:07 +0100

    chore: update license headers

--- schema.sql
+++ schema.sql
@@ -1,11 +1,10 @@
--- The AGPLv3 License (AGPLv3)
---
--- Copyright (c) 2023 Clément Joly
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+-- Copyright Clément Joly and contributors.
 --
 -- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU Affero General Public License as
--- published by the Free Software Foundation, either version 3 of the
--- License, or (at your option) any later version.
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
 --
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,7 +12,8 @@
 -- GNU Affero General Public License for more details.
 --
 -- You should have received a copy of the GNU Affero General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 CREATE TABLE IF NOT EXISTS Users (
 	-- Public TLS certificate, to authenticate a user
